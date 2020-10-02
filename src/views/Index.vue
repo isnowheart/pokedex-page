f2d85d06a5148a1e297bc92541fac2886e8e008d
isnowheart <isnowheart@icloud.com>
2020-10-02 13:28:35 -0300

    wip: Regions List

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,21 +1,30 @@
 <template lang="pug">
-div {{regionsList}}
+.container.grid-lg
+  .columns
+    .column.col-4.col-md-6.col-sm-12(v-for="region in regionsList.results")
+      .card
+        .card-header {{region.name}}
+        .card-body clque aqui
+
 </template>
+
+
 
 <script>
 import { mapState, mapActions } from "vuex"
 
 export default {
-    computed: { 
-        ...mapState('regions', ['regionsList']),
-        },
-        
+  computed: { 
+    ...mapState('regions', ['regionsList']),
+  },
+
     methods: { 
         ...mapActions('regions', ['fetchRegionsList']),
         },
 
     async created() {
         await this.fetchRegionsList()
+        console.log(this.regionsList)
     },
 }
 </script>
