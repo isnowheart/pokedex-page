69361bab8bc553b8f927f20a77783983fb9882db
isnowheart <isnowheart@icloud.com>
2020-10-02 23:46:54 -0300

    add filter in region list

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,30 +1,35 @@
 <template lang="pug">
 .container.grid-lg
+  filter-field(@search="filterList")
   .columns
-    .column.col-4.col-md-6.col-sm-12(v-for="region in regionsList.results")
-      .card
-        .card-header {{region.name}}
-        .card-body clque aqui
-
+    .column.col-4.col-md-6.col-sm-12(v-for="region in filteredRegionsList.results" :key="region.name")
+      region-row(:region="region")
 </template>
 
 
 
 <script>
 import { mapState, mapActions } from "vuex"
+import RegionRow from './components/RegionRow'
+import FilterField from '@/components/FilterField'
 
 export default {
   computed: { 
-    ...mapState('regions', ['regionsList']),
+    ...mapState('regions', ['filteredRegionsList']),
   },
 
     methods: { 
-        ...mapActions('regions', ['fetchRegionsList']),
+        ...mapActions('regions', ['fetchRegionsList', 'filterRegionsList']),
+
+      filterList(textToFilter) {
+        this.filterRegionsList(textToFilter)
+          },
         },
 
     async created() {
         await this.fetchRegionsList()
-        console.log(this.regionsList)
     },
+
+    components: { RegionRow, FilterField},        
 }
 </script>
