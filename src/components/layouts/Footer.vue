7000e994a3713147dcbeeb36c6c11229765767fd
isnowheart <isnowheart@icloud.com>
2020-10-06 17:38:31 -0300

    add footer

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.text-ellipsis
+  .divider
+    footer.text-dark
+      a(href="https://github.com/isnowheart/pokedex-page" target="_blank") Pokedex-page
+        code
+        small (v{{version}})
+</template>
+
+<script>
+import { version } from '../../../package'
+export default {
+  data() {
+    return {
+      version,
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.text-ellipsis
+  background-color #fff
+  .text-dark
+    text-align right
+</style>
