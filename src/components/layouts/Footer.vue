663cdf322285ebea6707d3abd584154da9a72bc5
isnowheart <isnowheart@icloud.com>
2020-10-07 00:38:14 -0300

    add sound bg

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
 .text-ellipsis
+  audio(volume=0.5 play controls autoplay loop 
+  src="../../assets/audio/Pokemon_Theme_on_Guitar.mp3")
+  av-media(media="../../assets/audio/Pokemon_Theme_on_Guitar.mp3")
   .divider
   footer.text-dark
     a(href="https://github.com/isnowheart/pokedex-page" target="_blank") Pokedex-page
@@ -9,6 +12,8 @@
 
 <script>
 import { version } from '../../../package'
+import AudioVisual from 'vue-audio-visual'
+
 export default {
   data() {
     return {
@@ -16,6 +21,18 @@ export default {
     }
   }
 }
+</script>
+
+<script type="text/javascript" src="https://www.scmplayer.net/script.js" 
+data-config="{'skin':'skins/tunes/skin.css',
+'volume':50,'autoplay':true,'shuffle':false,
+'repeat':1,'placement':'bottom','showplaylist':false,
+'playlist':[{'title':'Original Pokemon Theme Singer Jason Paige In Studio Full Pokemon Theme Song',
+'url':'https://www.youtube.com/watch?v=fCkeLBGSINs'},
+{'title':'Pok%E9mon Theme on Guitar',
+'url':'https://www.youtube.com/watch?v=zxKeIAZT7Mw'}
+]
+}" >
 </script>
 
 <style lang="stylus" scoped>
